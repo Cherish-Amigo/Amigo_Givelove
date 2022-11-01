8e9204edafe61d11ed7d1f0b25bc635bdecc7947
bomin-1206 <gbm4655@naver.com>
2022-11-01 21:59:21 +0900

    Amigo-1101(DB)

--- amigo/client/src/containers/GLogin.js
+++ amigo/client/src/containers/GLogin.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import LoginImg from '../image/LoginImg.png';
 import BlackLogo from '../image/BlackLogo.png';
 import {useNavigate} from 'react-router-dom';
@@ -8,6 +9,30 @@ function Login() {
 
   let navigate=useNavigate();
 
+  const [Id, SetId] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const idHandler = (e) => {
+    e.preventDefault();
+    SetId(e.target.value);
+  };
+
+  const passwordHandler = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let body = {
+      id: Id,
+      pass: Password
+    };
+    axios
+      .post("/teamAuth/login", body)
+      .then((res) => console.log(res));
+  }
+
   return (
     <>
     <div id="loginpage">
@@ -15,11 +40,11 @@ function Login() {
         <div id="loginInput">
             <img src={BlackLogo} id="BlackLogo" alt="BlackLogo" />
             <p id="loginText">로그인</p>
-            <form action="http://localhost:5000/teamAuth/login" method="post">
+            <form onSubmit={submitHandler}>
               <div id="id">아이디</div>
-              <input></input>
+              <input type="text" name="id" value={Id} onChange={idHandler} ></input>
               <div id="password">비밀번호</div>
-              <input type="password"></input>
+              <input type="password" name="password" value={Password} onChange={passwordHandler} ></input>
               <br />
               <button id="loginButton" type="submit">로그인</button>
             </form>
--- amigo/client/src/containers/Gsignup.js
+++ amigo/client/src/containers/Gsignup.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 import BlackLogo from '../image/BlackLogo.png';
 import {useNavigate} from 'react-router-dom';
 import "./Gsignup.css";
@@ -7,32 +8,105 @@ function Gsignup() {
 
   let navigate=useNavigate();
 
+  const [Kname, setKname] = useState("");
+  const [Ename, setEname] = useState("");
+  const [Number, setNumber] = useState("");
+  const [Address, setAddress] = useState("");
+  const [Address2, setAddress2] = useState("");
+  const [Name, setName] = useState("");
+  const [Businessnum, setBusinessnum] = useState("");
+  const [Id, setId] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const knameHandler = (e) => {
+    e.preventDefault();
+    setKname(e.target.value);
+  }
+
+  const EnameHandler = (e) => {
+    e.preventDefault();
+    setEname(e.target.value);
+  }
+
+  const numberHandler = (e) => {
+    e.preventDefault();
+    setNumber(e.target.value);
+  }
+
+  const addressHandler = (e) => {
+    e.preventDefault();
+    setAddress(e.target.value);
+  }
+
+  const address2Handler = (e) => {
+    e.preventDefault();
+    setAddress2(e.target.value);
+  }
+
+  const nameHandler = (e) => {
+    e.preventDefault();
+    setName(e.target.value);
+  }
+
+  const businessnumHandler = (e) => {
+    e.preventDefault();
+    setBusinessnum(e.target.value);
+  }
+
+  const idHandler = (e) => {
+    e.preventDefault();
+    setId(e.target.value);
+  }
+
+  const passwordHandler = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    let body = {
+      kanme : Kname,
+      ename: Ename,
+      number: Number,
+      address : Address,
+      address2: Address2,
+      name: Name,
+      businessnum : Businessnum,
+      id: Id,
+      pass: Password
+    };
+    axios
+      .post("/teamAuth/join", body)
+      .then((res) => console.log(res));
+  }
+
   return (
     <>
       <div id="gsignup">
         <div id="ginput">
           <img src={BlackLogo} id="BlackLogo" alt="BlackLogo" />
           <p id="gsignupText">단체 회원가입</p>
-          <form action="http://localhost:5000/teamAuth/join" method="post">
+          <form onSubmit={submitHandler}>
             <div id="kname">기업 한국명</div>
-            <input></input>
+            <input name="kname" value={Kname} onChange={knameHandler}></input>
             <div id="ename">기업 영문명</div>
-            <input></input>
+            <input name="ename" value={Ename} onChange={EnameHandler}></input>
             <div id='number'>연락처</div>
-            <input placeholder="'-'포함하여 작성"></input>
+            <input placeholder="'-'포함하여 작성" name='number' value={Number} onChange={numberHandler}></input>
             <div id='address'>주소</div>
-            <input id="addressInput"></input>
+            <input id="addressInput" name="address" value={Address} onChange={addressHandler}></input>
             <button class="ckbutton">주소찾기</button> <br />
-            <input placeholder='상세주소'></input>
+            <input placeholder='상세주소' className='address2' name="address2" value={Address2} onChange={address2Handler}></input>
             <div id="name">대표자명</div>
-            <input placeholder="한국지사 대표자명 입력"></input>
+            <input placeholder="한국지사 대표자명 입력" name="name" value={Name} onChange={nameHandler}></input>
             <div id="businessNum">사업자 등록번호</div>
-            <input placeholder="'-'포함하여 작성"></input>
+            <input placeholder="'-'포함하여 작성" name="businessnum" value={Businessnum} onChange={businessnumHandler}></input>
             <div id="gid">아이디</div>
-            <input id="idInput"></input>
+            <input id="idInput" name="id" value={Id} onChange={idHandler}></input>
             <button class="ckbutton">중복확인</button>
             <div id="gpw">비밀번호</div>
-            <input type="password"></input>
+            <input type="password" name="password" value={Password} onChange={passwordHandler}></input>
             <br />
             <button id="signup">회원가입</button>
           </form>
